0fe122a7687879467cea59047b77a48615c66fad
Roland Hostettler <roland.hostettler@aalto.fi>
2018-03-22 08:35:15 +0200

    Added SE kernel example, renamed examples.

---
+++ examples/example_se_ss.m
@@ -0,0 +1,108 @@
+% GP regression using state-space formulation and SE kernel
+%
+% Implementation of Gaussian Process (GP) regression based on the state-
+% space formulation of temporal GPs (time-series), see [1], [2].
+%
+% [1] J. Hartikainen and S. Sarkka, "Kalman filtering and smoothing 
+%     solutions to temporal Gaussian process regression models," in IEEE 
+%     International Workshop on Machine Learning for Signal Processing 
+%     (MLSP), pp. 379?384, August 2010.
+%
+% [2] A. Carron, M. Todescato, R. Carli, L. Schenato, and G. Pillonetto, 
+%     "Machine learning meets Kalman filtering," in 55th IEEE Conference on
+%     Decision and Control (CDC), pp. 4594? 4599, December 2016.
+% 
+% example_se_ss.m -- 2018-03-22
+% Roland Hostettler <roland.hostettler@aalto.fi>
+
+% Housekeeping
+clear variables;
+addpath ../src;
+
+%% Parameters
+sigma2 = 0.5;   % Variance
+ell = 1;        % Length scale
+J = 6;          % State-space approximation order 
+T = 5;          % Simulation time
+Ts = 0.5;       % Sampling time
+R = 0.01;       % Measurement noise
+
+%% Covariance Approximation
+[A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, J);
+
+% Visualize the true spectral density and its approximation
+w = (0:0.1:5);
+Sww = sigma2*sqrt(2*pi*ell^2)*exp(-w.^2*ell^2/2);
+s = tf('s');
+G = C*(s*eye(size(A))-A)^(-1)*B;
+[Sww_hat, ~] = bode((G*G')*Sw, w);
+
+figure(1); clf();
+plot(w, Sww); hold on;
+plot(w, squeeze(Sww_hat));
+legend('True', 'Approximation');
+title('Comparison of the Spectral Densities');
+xlabel('\omega / s^{-1}'); ylabel('S_{ww}(\omega)');
+
+%% Data Generation
+k = @(t1, t2) gpk_se(t1, t2, ell, sigma2); %sigma2*exp(-(t2-t1).^2/(2*ell^2));
+t = (Ts:Ts:T);
+Ktt = gp_calculate_covariance(t, k);
+N = length(t);
+fr = chol(Ktt).'*randn(N, 1);
+yr = fr + sqrt(R)*randn(N, 1);
+
+%% Batch Regression
+tic;
+S = Ktt + R*eye(N);
+fhat_batch = Ktt/S*yr;
+toc;
+
+%% KF Regression
+% Discretization
+[F, Q] = lti_disc(A, B, Sw, Ts); 
+Q = (Q+Q')/2;
+Lq = chol(Q).';
+
+% Filtering
+tic;
+fhat_f = zeros(N, 1);
+m = zeros(J, 1);
+mf = zeros(J, N);
+P = Pinf;
+Pf = zeros(J, J, N);
+for n = 1:N
+    % Prediction
+    mp = F*m;
+    Pp = F*P*F' + Q;
+    
+    % Update
+    S = C*Pp*C' + R;
+    K = Pp*C'/S;
+    m = mp + K*(yr(n) - C*mp);
+    P = Pp - K*S*K';
+    
+    mf(:, n) = m;
+    Pf(:, :, n) = P;
+    
+    % Store
+    fhat_f(n) = C*m;
+end
+
+% Smoothing
+[ms, Ps] = rts_smooth(mf, Pf, F, Q);
+fhat_s = C*ms;
+toc;
+
+%% Results
+fprintf('Batch RMSE: %.3f\n', rms(fr-fhat_batch));
+fprintf('KF RMSE: %.3f\n', rms(fr-fhat_f));
+fprintf('RTS RMSE: %.3f\n', rms(fr-fhat_s.'));
+
+figure(2); clf();
+plot(t, fr); hold on;
+plot(t, fhat_batch);
+plot(t, fhat_f);
+plot(t, fhat_s);
+legend('True', 'Batch', 'KF', 'RTS');
+title('Estimated GP Values');
--- src/gpk_se_ss.m
+++ src/gpk_se_ss.m
@@ -1,8 +1,8 @@
-function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, N)
+function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, J)
 % State space representation of the SE covariance function (Taylor approx.)
 %
 % USAGE
-%   [A, B, C, Sw, Pinf] = se2ss(l, sigma, N)
+%   [A, B, C, Sw, Pinf] = se2ss(ell, sigma2, J)
 %
 % DESCRIPTION
 %   Calculates the linear state-space approximation of a temporal Gaussian 
@@ -13,7 +13,7 @@ function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, N)
 %
 %   where
 %
-%       k(t, t') = sigma^2*exp(-(t-t')^2/(2*l^2))
+%       k(t, t') = sigma2*exp(-(t-t')^2/(2*ell^2))
 %
 %   is the SE covariance kernel, then it calculates the matrices A, B, C
 %   and spectral density Sw such that
@@ -22,7 +22,7 @@ function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, N)
 %       f = C*x
 %
 %   where Sw is the spectral density of w(t). Furthermore, Pinf is the
-%   stationary covariance and solves the continuous Ricatti equation
+%   stationary covariance and solves the continuous Lyapunov equation
 %
 %       A'*Pinf + Pinv*A + B*Sw*B' = 0.
 %
@@ -33,7 +33,7 @@ function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, N)
 %   ell     GP length scale (optional, default: 1).
 %   sigma2  GP covariance magnitude (standard deviation; optional, 
 %           default: 1).
-%   N       Approximation order (must be even; optional, default: 6).
+%   J       Approximation order (must be even; optional, default: 6).
 %
 % RETURNS
 %   A, B, C, Sw, Pinf
@@ -43,7 +43,7 @@ function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, N)
 %   gpk_matern_ss
 %
 % REFERENCES
-%   [1] J. Hartikainen and S. S?rkk?, "Kalman filtering and smoothing 
+%   [1] J. Hartikainen and S. Sarkka, "Kalman filtering and smoothing 
 %       solutions to temporal Gaussian process regression models," in IEEE 
 %       International Workshop on Machine Learning for Signal Processing 
 %       (MLSP), pp. 379?384, August 2010.
@@ -62,29 +62,29 @@ function [A, B, C, Sw, Pinf] = gpk_se_ss(ell, sigma2, N)
     if nargin < 2 || isempty(sigma2)
         sigma2 = 1;
     end
-    if nargin < 3 || isempty(N)
-        N = 6;
+    if nargin < 3 || isempty(J)
+        J = 6;
     end
-    if mod(N, 2) ~= 0
-        error('The approximation order N must be even (%d).', N);
+    if mod(J, 2) ~= 0
+        error('The approximation order J must be even (%d).', J);
     end
 
     %% Approximation
-    n = 0:N;
+    j = 0:J;
     kappa = 1/(2*ell^2);
-    Sw = sigma2*factorial(N)*(4*kappa)^N*sqrt(pi/kappa);
+    Sw = sigma2*factorial(J)*(4*kappa)^J*sqrt(pi/kappa);
     
-    c = zeros(1, 2*N+1);
-    c(2*n+1) = factorial(N)*(-1).^n.*(4*kappa).^(N-n)./factorial(n);
+    c = zeros(1, 2*J+1);
+    c(2*j+1) = factorial(J)*(-1).^j.*(4*kappa).^(J-j)./factorial(j);
     r = roots(fliplr(c));
     rminus = r(real(r) < 0);
     pminus = fliplr(poly(rminus));
 
     A = [
-        zeros(N-1, 1), eye(N-1);
-                   -pminus(1:N);
+        zeros(J-1, 1), eye(J-1);
+                   -pminus(1:J);
     ];
-    B = [zeros(1, N-1), 1].';
-    C = [1, zeros(1, N-1)];
-    Pinf = care(A', zeros(N), B*Sw*B');
+    B = [zeros(1, J-1), 1].';
+    C = [1, zeros(1, J-1)];
+    Pinf = care(A', zeros(J), B*Sw*B');
 end
