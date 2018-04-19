5f69e21d5c442b06b0aab3715a5a4bf705e6b6fc
Roland Hostettler <roland.hostettler@aalto.fi>
2018-04-19 09:43:10 +0300

    Updated gp_model_ss, README.md.

--- README.md
+++ README.md
@@ -1,31 +1,47 @@
 Gaussian Process Library
 ========================
-This is a simple Gaussian process (GP) library for Matlab. It provides a bunch of simple functions to do basic GP regression and classification, mostly aimed for research purposes. The library is by no means complete or stable and is most likely not suitable for generic purposes.
+This is a simple Gaussian process (GP) library for Matlab. It provides a bunch of simple functions to do basic GP regression and classification, mostly aimed for research purposes. The library is by no means complete or stable and is most likely not suitable for generic purposes. If you are looking for a stable GP library, have a look at (for example) [GPStuff](http://research.cs.aalto.fi/pml/software/gpstuff/).
+
+
+Naming Conventions
+------------------
+This library follows a couple of naming conventions. These are:
+
+* `gp_XXX`: Generic function (e.g. `gp_calculate_covariance()` or `gp_predict()`),
+* `gpk_XXX`: Covariance functions (e.g. `gpk_se()` or `gpk_matern()`),
+* `gpc_XXX`: Functions related to GP classification (e.g. `gpc_train_ep()` or `gpc_predict_ep()`).
+
+Functions not following this convention are most likely broken.
 
 
 Covariance Functions
 --------------------
-Covariance kernels follow the general interface `Kx1x2 = gpk_xxx(x1, x2, theta1, theta2, ...)`, where `theta1`, `theta2`, etc. denote the kernels parameters. If omitted, the kernel parameters will be set to a default value (`1` in most cases, see the Matlab help text for the respective covariance function). In addition to the standard formulation of the covariance function, there are also functions for the spectral density (generally of the form `Sw = gpk_xxx_psd(w, theta1, theta2, ...)`) and state space representations (of the form `[A, B, C, Sw, Pinf] = gpk_xxx_ss(theta1, theta2, ...)`) for stationary covariance functions.
+Covariance kernels follow the general interface `Kxx = gpk_xxx(x1, x2, theta1, theta2, ...)`, where `theta1`, `theta2`, etc. denote the kernels parameters. If omitted, the kernel parameters will be set to a default value (`1` in most cases, see the Matlab help text for the respective covariance function). In addition to the standard formulation of the covariance function, there are also functions for the spectral density (generally of the form `Sw = gpk_xxx_psd(w, theta1, theta2, ...)`) and state-space representations (of the form `[A, B, C, Sw, Pinf] = gpk_xxx_ss(theta1, theta2, ...)`) for stationary covariance functions.
 
 The following covariance functions are implemented:
 
 * Squared exponential covariance function:
     * `gpk_se(x1, x2, ell, sigma2)`: Covariance function,
     * `gpk_se_psd(w, ell, sigma2)`: Power spectral density,
-    * `gpk_se_ss(ell, sigma2, N)`: State space representation.
+    * `gpk_se_ss(ell, sigma2, N)`: State-space representation.
 * Matérn covariance function:
     * `gpk_matern(x1, x2, ell, sigma2, nu)`: Covariance function,
-    * `gpk_matern_ss(ell, sigma2, nu)`: State space representation.
-*
+    * `gpk_matern_ss(ell, sigma2, nu)`: State-space representation.
 
-In addition to the standard formulation
+
+State-Space Representations
+---------------------------
+One of the main purposes of the library is to provide tools for state-space representations of temporal GPs. Hence, there are a couple of functions that help to build this kind of models. These are:
+
+* `gp_model_ss()`: A constructor for discrete-time state-space representations of GPs with separable covariance functions.
+
 
 Examples
 --------
 There are a bunch of examples implemented (see the folder `examples`). These are:
 
-* `ex_gpc_toy.m`: Gaussian process classification example using the Laplace and Expectation Propagation approximations (re-implementation of the example in Section 3.7.2 of Rasmussen and Williams (2006)).
-* `ex_matern_ss.m`: Example of using the Matern state space formulation for a spatio-temporal GP regression problem.
+* `example_gpc_toy.m`: Gaussian process classification example using the Laplace and Expectation Propagation approximations (re-implementation of the example in Section 3.7.2 of Rasmussen and Williams (2006)).
+* `example_matern_ss.m`: Example of using the Matern state space formulation for a spatio-temporal GP regression problem.
 
 
 TODO
@@ -35,5 +51,40 @@ TODO
 [ ] Allow for diagonal length scale matrix in gpk_se.
 [ ] Fix minor bugs in gpk_matern.
 [ ] Adopt the naming convention gpk_xx
-[ ] Make a github repository with everything that is needed
+
+
+Status
+------
+Here's an overview of the status of the different files:
+
+*examples:*
+* `example_gpc_toy.m`
+* `example_matern_ss.m`
+* `example_model_ss.m`
+* `example_se_ss.m`
+
+*src:*
+* `dk_periodic.m`
+* `gp_calculate_cross_covariance.m`
+* `gp_calculate_log_posterior.m`
+* `gpc_predict_ep.m`
+* `gpc_train_laplace.m`
+* `gpk_matern_ss.m`
+* `gpk_se.m`
+* `gpk_se_ss.m`
+* *OK* `gp_model_ss.m`
+* `gp_sample.m`
+* `k_canonical_periodic_ss.m`
+* `k_quasiperiodic_ss.m`
+* `gp_calculate_covariance.m`
+* `gp_calculate_loglikelihood_ss.m`
+* `gp_calculate_logposterior_ss.m`
+* `gpc_train_ep.m`
+* `gpk_matern.m`
+* `gp_k_quasiperiodic_ss.m`
+* `gpk_se_psd.m`
+* `gp_model_smc.m`
+* `gp_predict.m`
+* `k_bessel.m`
+* `k_periodic.m`
 
--- src/gp_model_ss.m
+++ src/gp_model_ss.m
@@ -1,19 +1,24 @@
-function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
-% State-space Gaussian process model with arbitrary likelihood
+function [F, Q, C, m0, P0, Nx] = gp_model_ss(gpk_t, gpk_u, ut, up, Ts)
+% Discrete-time state-space model for separable spatio-temporal GPs
 % 
 % USAGE
-%   model = GP_MODEL_SS()
-%   model = GP_MODEL_SS(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
+%   [F, Q, C, m0, P0] = GP_MODEL_SS()
+%   [F, Q, C, m0, P0, Nx] = GP_MODEL_SS(gpk_t, gpk_u, ut, up, Ts)
 %
 % DESCRIPTION
-%   Implements a generic state-space representation for latent Gaussian
-%   processes with arbitrary likelihoods. The model is useful together with
-%   SMC-based methods, see [1]. When Kalman filters are to be used, this
-%   model constructor is not useful.
+%   Generic discrete-time state-space representation for temporal and
+%   spatio-temporal Gaussian processes with separable covariance function.
+%   The model is of the form
 %
-%   Both temporal and spatio-temporal models can be constructed using this
-%   routine. For spatio-temporal models, the limitation right now is that
-%   only separable covariance functions are accepted.
+%       x[n] = F x[n-1] + q[n]
+%       f[n] = C x[n]
+%
+%   where x[0] ~ N(m0, P0) and q[n] ~ N(0, Q).
+%
+%   Note that if both training (ut) and prediction (up) locations are
+%   given, they are stacked such that the first 1...Nx*Nt states correspond
+%   to the states of the training locations (and their derivatives) and the
+%   states Nx*Nt+1...Nx*Nt+Nx*Np correspond to the test locations.
 %
 % PARAMETERS
 %   gpk_t   Temporal covariance function (state-space representation; 
@@ -22,16 +27,14 @@ function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
 %   ut      Inducing (training) points for the spatial variable. Only
 %           required if gpk_u is not empty.
 %   up      Prediction points for the spatial variable (default: []).
-%   py      Measurement likelihood structure. Must follow the common pdf 
-%           format (default: linear, Gaussian likelihood with unit 
-%           variance).
-%   ptheta  (Hyper)parameter prior, cell array with one prior for each
-%           parameter in the order of the parameter vector (defualt: {}).
-%           The pdf descriptions must follow the common pdf format.
 %   Ts      Sampling time (default: 1).
 %
 % RETURNS
-%   model   The model structure.
+%   F, Q, C, m0, P0
+%           Discrete-time linear state-space model parameters.
+%   Nx      For temporal models, this is the state dimension and for
+%           spatio-temporal models, this is the dimension of the state for
+%           one spatial inducing point.
 %
 % REFERENCES
 %   [1] R. Hostettler, S. Sarkka, S. J. Godsill, "Rao-Blackwellized
@@ -39,20 +42,14 @@ function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
 %       processes," in 27th IEEE International Workshop on Machine Learning
 %       for Signal Processing (MLSP), Tokyo, Japan, September 2017.
 %
-% VERSION
-%   2018-03-22
-%
 % AUTHOR
-%   Roland Hostettler <roland.hostettler@aalto.fi>
+%   2018-03-22 -- Roland Hostettler <roland.hostettler@aalto.fi>
 
 % TODO:
 %   * Allow for non-zero initial states
-%   * Use a generic CLGSS model once that is fully implemented rather than
-%     re-implementing 
-%   * Use a struct-generating function for normal pdfs
 
     %% Defaults
-    narginchk(0, 7)
+    narginchk(0, 5)
     if nargin < 1 || isempty(gpk_t)
         gpk_t = @gpk_matern_ss;
     end
@@ -67,13 +64,7 @@ function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
             up = [];
         end
     end
-    if nargin < 5
-        py = [];
-    end
-    if nargin < 6 || isempty(ptheta)
-        ptheta = {};
-    end
-    if nargin < 7 || isempty(Ts)
+    if nargin < 5 || isempty(Ts)
         Ts = 1;
     end
         
@@ -82,14 +73,14 @@ function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
     [A, B, C, Sw, P0] = gpk_t();
     [F, Q] = lti_disc(A, B, Sw, Ts);
     Q = (Q+Q')/2;
-    N0 = size(F, 1);
+    Nx = size(F, 1);
         
     % Add spatial part, if applicable
     if ~temporal
         % Generate a system for each point uj
-        Jt = size(ut, 2);
-        Jp = size(up, 2);
-        J =  Jt + Jp;
+        Nt = size(ut, 2);
+        Np = size(up, 2);
+        J =  Nt + Np;
         u = [ut, up];
     
         % State transition matrix: A block-diagonal matrix with the matrix
@@ -98,7 +89,7 @@ function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
     
         % Measurement vector: A block vector similar to the above but with 
         % no measurements for the prediction points up.
-        C = [kron(eye(Jt), C), zeros(Jt, Jp*N0)];
+        C = [kron(eye(Nt), C), zeros(Nt, Np*Nx)];
 
         % The process noise covariance between the ujs' subsystems is given
         % by k(u, u')*Q. Hence, the Kronecker produc between the full Kuu
@@ -107,62 +98,8 @@ function model = gp_model_ss(gpk_t, gpk_u, ut, up, py, ptheta, Ts)
         Kuu = gp_calculate_covariance(u, gpk_u);
         Q = kron(Kuu, Q);
         P0 = kron(Kuu, P0);
-    else
-        Jt = 1;
-        Jp = 0;
     end
     
-    % Default likelihood
-    if isempty(py)
-        py = struct();
-        py.fast = true;
-        py.logpdf = @(y, s, t) logmvnpdf(y.', s.', eye(Jt)).';
-    end
-    
-    %% Create model structure
-    % Calculate the indices to the nonlinear and linear state components.
-    % Only the training (ut) points are nonlinear (these are the states
-    % that enter the likelihood).
-    Nx = Jt*N0 + Jp*N0;
-    in = (1:N0:Jt*N0-1).';          % Indices of the nonlinear states
-    il = (2:N0).'*ones(1, Jt);      % Indices of the linear states
-    a = ones(N0-1, 1)*(0:N0:(Jt-1)*N0);
-    il = a(:) + il(:);
-    il = [il; (Jt*N0+1:Nx).'];
-                       
-    % Initialize model struct
-    model = struct();
-    model.in = in;
-    model.il = il;
-
     % Initial state
-    m0 = zeros(N0, 1);  
-    LP0 = chol(P0).';
-    model.px0 = struct();
-    model.px0.fast = true;
-    model.px0.rand = @(M) m0*ones(1, M) + LP0*randn(N0, M);
-    model.px0.logpdf = @(x) logmvnpdf(x.', (m0*ones(1, size(x, 2))).', P0).';
-    model.m0 = m0;
-    model.P0 = P0;
-    
-    % Dynamic model
-    LQ = chol(Q).';
-    model.fn = @(s, t) F(in, in)*s;
-    model.fl = @(s, t) F(il, in)*s;
-    model.Fn = @(s, t) F(in, il);
-    model.Fl = @(s, t) F(il, il);
-    model.F = @(t) F;
-    model.Q = @(s, t) Q;
-    model.Qn = @(s, t) Q(in, in);
-    model.Ql = @(s, t) Q(il, il);
-    model.Qnl = @(s, t) Q(in, il);
-    model.px = struct();
-    model.px.fast = true;
-    model.px.rand = @(x, t) F*x + LQ.'*randn(N0, size(x, 2));
-    model.px.logpdf = @(xp, x, t) logmvnpdf(xp.', (F*x).', Q).';
-
-    % Likelihood and parameter prior; these need to be defined outside (or
-    % the default is a linear, Gaussian likelihood, see above.
-    model.py = py;
-    model.ptheta = ptheta;
+    m0 = zeros(size(F, 1), 1);
 end
